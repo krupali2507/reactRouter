023c922c64a3edbfce98df7ed284ecab02e6a4c0
Krupali <krupali.igenerate@gmail.com>
2024-03-23 17:55:54 +0530

    basic of react router added

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from "react";
+import "./App.css";
+import Footer from "./components/Footer/Footer";
+import Header from "./components/Header/Header.jsx";
+import Home from "./components/Home/Home";
+
+function App() {
+  return (
+    <>
+      <Header />
+      {/* <Home />
+      <Footer /> */}
+    </>
+  );
+}
+
+export default App;
---
+++ src/main.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+import App from "./App.jsx";
+import About from "./components/About/About.jsx";
+import Contact from "./components/Contact/contact.jsx";
+import Home from "./components/Home/Home.jsx";
+import "./index.css";
+import Layout from "./Layout.jsx";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<Layout />}>
+      <Route path="" element={<Home />} />
+      <Route path="about" element={<About />} />
+      <Route path="contact-us" element={<Contact />} />
+    </Route>
+  )
+);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
